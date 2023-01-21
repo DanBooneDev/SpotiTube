d280daf5df03f9988cf036943f2fff688711863a
DanBooneDev <dan.golembiewski@gmail.com>
2023-01-21 10:59:20 -0500

    added get playlists button

--- js/home-page.js
+++ js/home-page.js
@@ -10,17 +10,18 @@ const scopes = "user-library-read";
 // The Spotify Web API endpoint for retrieving playlists
 const playlists_endpoint = 'https://api.spotify.com/v1/me/playlists';
 
-
 const spotify_login_button = document.getElementById("spotify-login-button");
+const spotify_playlists_button = document.getElementById("spotify-playlists-button");
 
 spotify_login_button.addEventListener("click", function(){
-    console.log("spotify login button pressed");
+    
+  console.log("spotify login button pressed");
     // Redirect the user to the Spotify Authorization URL
     window.location.href = `https://accounts.spotify.com/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scopes}&response_type=code`;
 
 });
 
-function getSpotifyPlaylists(){
+spotify_playlists_button.addEventListener("click", function(){
   // Use axios to send a GET request to the Spotify Web API
   axios.get(playlists_endpoint, {
     headers: {
@@ -33,4 +34,6 @@ function getSpotifyPlaylists(){
     // Log the error
     console.error(error);
   });
-}
+
+});
+
